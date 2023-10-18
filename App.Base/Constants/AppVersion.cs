24522f5217a53e73a084102fe0afb1bceea69e29
abinashkalikote <lkalikote1@gmail.com>
2023-10-18 16:28:17 +0545

    update

---
+++ App.Base/Constants/AppVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Base.Constants
+{
+    public static class AppVersion
+    {
+        public static string Version { get; set; } = "V1.0";
+    }
+}

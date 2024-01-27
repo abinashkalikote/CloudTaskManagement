4c58e045e7e1ad99e9d36faa12163a389449bd56
abinashkalikote <lkalikote1@gmail.com>
2024-01-27 22:14:49 +0545

    feat : add hash extension

---
+++ App.Base/Extensions/PasswordHashExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Base.Extensions;
+
+public static class PasswordHashExtension
+{
+    public static string Hash(this string text)
+    {
+        return BCrypt.Net.BCrypt.EnhancedHashPassword(text);
+    }
+}
